b7f0ae81a92b8102954beea69ee4e7d58b979ea4
Mostaque Ahmed <noyon25@hotmail.com>
2021-08-19 11:10:33 -0500

    exam

---
+++ Exam1/Tasks/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ConvertTime("12:30 pm");
+            Console.WriteLine(result);
+        }
+
+        public static string ConvertTime(string twelveHourFormat)
+        {
+            return DateTime.Parse(twelveHourFormat).ToString("HH:mm"); 
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
